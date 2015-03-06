e8e5c7f6b27b44c0d7b550f0bbe5735835f8fb67
Henry0422 <ronglihan@Ronglis-MacBook-Pro.local>
2015-03-06 01:47:04 -0600

    Add Create Table AddressBook

--- AzureTest.php
+++ AzureTest.php
@@ -16,7 +16,26 @@
 			}
 			catch(Exception $e){
 				die("Connection failed: ".print_r($e));
-				}
+			}
+
+			$sql = "CREATE TABLE AddressBook(
+				id INT AUTO_INCREMENT PRIMARY KEY,
+				firstname VARCHAR(30) NOT NULL,
+				lastname VARCHAR(30) NOT NULL,
+				company VARCHAR(30),
+				phone INT NOT NULL,
+				email VARCHAR(255) NOT NULL,
+				url VARCHAR(255),
+				address VARCHAR(255) NOT NULL,
+				birthday DATE,
+				add_date DATE NOT NULL,
+				note VARCHAR(255)
+			)";
+			
+			if($conn->query($sql) == TRUE)
+				echo "Table AddressBook created succefully";
+			else
+				echo "Error creating table: ".$conn->error;
 			
 		 ?> 
 	</body>
