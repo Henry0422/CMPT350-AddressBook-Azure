6bf12c78c91c8dc5f06989344305dedf34d9a99f
Henry0422 <ronglihan@uofs-10-227-129-162.usask.ca>
2015-03-07 14:53:34 -0600

    Test

--- AdminPage.php
+++ AdminPage.php
@@ -7,40 +7,22 @@
 		<a href='add.php'>
 			Add New Contact</br>
 		</a>
-
+		<table>
+			<tr>
+				<th>ID</th>
+				<th>First Name</th>
+				<th>Last Name</th>
+				<th>Company</th>
+				<th>Phone No.</th>
+				<th>Email</th>
+				<th>URL</th>
+				<th>Address</th>
+				<th>Birthday</th>
+				<th>Date</th>
+				<th>Note</th>
+			</tr>
 		<?php
-			echo "<table>
-				<tr>
-					<th>ID</th>
-					<th>First Name</th>
-					<th>Last Name</th>
-					<th>Company</th>
-					<th>Phone No.</th>
-					<th>Email</th>
-					<th>URL</th>
-					<th>Address</th>
-					<th>Birthday</th>
-					<th>Date</th>
-					<th>Note</th>
-				</tr>";
 
-			class TableRows extends RecursiveIteratorIterator { 
-			     function __construct($it) { 
-			         parent::__construct($it, self::LEAVES_ONLY); 
-			     }
-
-			     function current() {
-			         return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
-			     }
-
-			     function beginChildren() { 
-			         echo "<tr>"; 
-			     } 
-
-			     function endChildren() { 
-			         echo "</tr>" . "\n";
-			     } 
-			} 
 			$server = "tcp:gpntf5hrgo.database.windows.net,1433";
 			$user = "SQLAdmin";
 			$pwd = "henry0422!";
@@ -50,19 +32,43 @@
 				$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 				$stmt = $conn->prepare("SELECT * FROM AddressBook");
 				$stmt->execute();
-
-				$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-				foreach(new TableRows(new RecursiveIteratorIterator($stmt->fetchAll())) as $k=>$v){
-					echo $v;
+			}
+			catch(Exception $e){
+				die("Connection failed: ".print_r($e));
+			}
+			
+		    $id=$_GET['ContactID'];
+			$sql = "SELECT * FROM AddressBook";
+			$result = $conn->query($sql);
+		
+			if($result->rowCount()>0){
+				while($row = $result->fetch_assoc()){
+					echo "<tr>
+						<td>".$row["id"]."</td>
+						<td>".$row["firstname"]." </td>
+						<td>".$row["lastname"]."</td>
+						<td>".$row["company"]."</td>
+						<td>".$row["phone"]."</td>
+						<td>".$row["email"]."</td>
+						<td>".$row["url"]."</td>
+						<td>".$row["address"]."</td>
+						<td>".$row["birthday"]."</td>
+						<td>".$row["add_date"]."</td>
+						<td>".$row["note"]."</td>
+						<td>
+							<a href='update.php?ContactID=".$row["id"]."'>
+								Update
+							</a>
+								<a href='DeleteContact.php?ContactID=".$row["id"]."'
+								onclick='return confirm(\"Are you sure\")'>
+								Delete
+							</a>
+						</td>
+					</tr>";
 				}
 			}
-			catch(PDOException $e) {
-			     echo "Error: " . $e->getMessage();
-			}
-			$conn = null;
-			echo "</table>";
+				
 		 ?>
-		 
+		 </table>
 	</body>
 </html>
