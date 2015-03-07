0d86c45c7827084115f9b2f613c4f425e47498c4
Henry0422 <ronglihan@uofs-10-227-129-162.usask.ca>
2015-03-07 13:21:54 -0600

    Change AddContact.php

--- AddContact.php
+++ AddContact.php
@@ -5,28 +5,32 @@
 	</head>
 	<body>
 		<?php
-			$servername="lovett.usask.ca";
-		    $username="cmpt350_roh919";
-		    $password="npig97rako";
-			$dbname="cmpt350_roh919";
-			
-		    $conn = new mysqli($servername, $username, $password, $dbname);
-
-		    if($conn->connect_error)
-				die("Connection failed".$conn->connect_error);
-		    else
+			$server = "tcp:gpntf5hrgo.database.windows.net,1433";
+			$user = "SQLAdmin";
+			$pwd = "henry0422!";
+			$db = "Assignment2";
+			try{
+				$conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
+				$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 				echo "Connection successfully</br>";
-				
-			$sql = "INSERT INTO AddressBook(firstname, lastname, company, phone, email, url, address, birthday, add_date)
-					VALUES ('".$_POST['fname']."', '".$_POST['lname']."','".$_POST['company']."', '".$_POST['phone']."',
-					'".$_POST['email']."','".$_POST['url']."', '".$_POST['address']."', '".$_POST['birthday']."', 
-					'".$_POST['add_date']."')";
-					
-			if($conn->query($sql) == TRUE)
-				echo "New address contact Create successfully: ".$sql.
-				"<br/>with id:".$conn->insert_id."<br/>";
-			else
-				echo "Error with query: ".$sql."<br/>".$conn->error;
+			}
+			catch(Exception $e){
+				die("Connection failed: ".print_r($e));
+			}
+			
+			try{
+				$sql = "INSERT INTO AddressBook(firstname, lastname, company, phone, email, url, address, birthday, add_date)
+						VALUES ('".$_POST['fname']."', '".$_POST['lname']."','".$_POST['company']."', '".$_POST['phone']."',
+						'".$_POST['email']."','".$_POST['url']."', '".$_POST['address']."', '".$_POST['birthday']."', 
+						'".$_POST['add_date']."')";
+				$conn->exec($sql);
+				$last_id = $conn->lastInsertId();
+				echo "New record created successfully. Last inserted ID is: ".$last_id;
+				}
+			catch(){
+				echo $sql."<br>".$e->getMessage();
+			}
+
 		 ?>
 		 <a href='AdminPage.php'>
 			Go to mainpage</br>
