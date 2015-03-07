4c5a414782880d50ba33f8e69176bb8e82005650
Henry0422 <ronglihan@uofs-10-227-129-162.usask.ca>
2015-03-07 15:38:47 -0600

    Change SelectTest.php

--- SelectTest.php
+++ SelectTest.php
@@ -1,38 +1,53 @@
 <!DOCTYPE html>
-
 <html>
-	<head>
-	</head>
-	<body>
-		<a href='add.php'>
-			Add New Contact</br>
-		</a>
-		<?php
+<body>
 
-			$server = "tcp:gpntf5hrgo.database.windows.net,1433";
-			$user = "SQLAdmin";
-			$pwd = "henry0422!";
-			$db = "Assignment2";
-			try{
-				$conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
-				$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-				$stmt = $conn->prepare("SELECT * FROM AddressBook");
-				$stmt->execute();
-			}
-			catch(Exception $e){
-				die("Connection failed: ".print_r($e));
-			}
-			
-		    //$id=$_GET['ContactID'];
-		    try{
-			    $sql = "SELECT * FROM AddressBook";
-				$result = $conn->query($sql);
-			
-		    }
-			catch(Exception $e){
-				echo $sql."<br>".$e->getMessage();
-			}
-				
-		 ?>
-	</body>
+<?php
+echo "<table style='border: solid 1px black;'>";
+  echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator { 
+     function __construct($it) { 
+         parent::__construct($it, self::LEAVES_ONLY); 
+     }
+
+     function current() {
+         return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+     }
+
+     function beginChildren() { 
+         echo "<tr>"; 
+     } 
+
+     function endChildren() { 
+         echo "</tr>" . "\n";
+     } 
+} 
+
+	$servername = "tcp:gpntf5hrgo.database.windows.net,1433";
+	$username = "SQLAdmin";
+	$password = "henry0422!";
+	$dbname = "Assignment2";
+
+try {
+     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $stmt = $conn->prepare("SELECT * FROM AddressBook"); 
+     $stmt->execute();
+
+     // set the resulting array to associative
+     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+     foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+         echo $v;
+     }
+}
+catch(PDOException $e) {
+     echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>  
+
+</body>
 </html>
