906bd90857b1f556629e13b9151448c6699bef8e
Henry0422 <ronglihan@Ronglis-MacBook-Pro.local>
2015-03-07 18:16:08 -0600

    Change for Test

--- AdminPage.php
+++ AdminPage.php
@@ -1,68 +1,71 @@
 <!DOCTYPE html>
+
 <html>
-<body>
+    <head>
+    </head>
+    <body>
+        <a href='add.php'>
+            Add New Contact</br>
+        </a>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Company</th>
+                <th>Phone No.</th>
+                <th>Email</th>
+                <th>URL</th>
+                <th>Address</th>
+                <th>Birthday</th>
+                <th>Date</th>
+                <th>Note</th>
+            </tr>
+        <?php
+            $server = "tcp:gpntf5hrgo.database.windows.net,1433";
+            $user = "SQLAdmin";
+            $pwd = "henry0422!";
+            $db = "Assignment2";
+            try{
+                $conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
+                $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+                echo "Connection successfully</br>";
+            }
+            catch(Exception $e){
+                die("Connection failed: ".print_r($e));
+            }
+                
+            $id=$_GET['ContactID'];
+            $sql = "SELECT * FROM AddressBook";
+            $result = $conn->query($sql);
+        
+            while($row = $row = $result->fetch(PDO::FETCH_ASSOC) ){
+                echo "<tr>
+                    <td>".$row["id"]."</td>
+                    <td>".$row["firstname"]." </td>
+                    <td>".$row["lastname"]."</td>
+                    <td>".$row["company"]."</td>
+                    <td>".$row["phone"]."</td>
+                    <td>".$row["email"]."</td>
+                    <td>".$row["url"]."</td>
+                    <td>".$row["address"]."</td>
+                    <td>".$row["birthday"]."</td>
+                    <td>".$row["add_date"]."</td>
+                    <td>".$row["note"]."</td>
+                    <td>
+                        <a href='update.php?ContactID=".$row["id"]."'>
+                            Update
+                        </a>
+                        <a href='DeleteContact.php?ContactID=".$row["id"]."'
+                        onclick='return confirm(\"Are you sure\")'>
+                        Delete
+                        </a>
+                    </td>
+                </tr>";
+            }
 
-<?php
-echo "<table style='border: solid 1px black;'>";
-  echo "<tr>
-				<th>ID</th>
-				<th>First Name</th>
-				<th>Last Name</th>
-				<th>Company</th>
-				<th>Phone No.</th>
-				<th>Email</th>
-				<th>URL</th>
-				<th>Address</th>
-				<th>Birthday</th>
-				<th>Date</th>
-				<th>Note</th>
-		</tr>";
-
-class TableRows extends RecursiveIteratorIterator { 
-     function __construct($it) { 
-         parent::__construct($it, self::LEAVES_ONLY);
-         echo "<a href='update.php?ContactID=".$row["id"]."'>Update</a>".
- 				"<a href='DeleteContact.php?ContactID=".$row["id"].
- 				"'onclick='return confirm(\"Are you sure\")'>Delete</a>";
-     }
-
-     function current() {
-         return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
-     }
-
-     function beginChildren() { 
-         echo "<tr>"; 
-     } 
-
-     function endChildren() { 
-         echo "</tr>"."\n";
-     } 
-} 
-
-	$servername = "tcp:gpntf5hrgo.database.windows.net,1433";
-	$username = "SQLAdmin";
-	$password = "henry0422!";
-	$dbname = "Assignment2";
-
-try {
-     $conn = new PDO("sqlsrv:Server= $servername ; Database = $dbname ", $username, $password);
-     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     $stmt = $conn->prepare("SELECT * FROM AddressBook"); 
-     $stmt->execute();
-
-     // set the resulting array to associative
-     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-
-     foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
-         echo $v;
-     }
-}
-catch(PDOException $e) {
-     echo "Error: " . $e->getMessage();
-}
-$conn = null;
-echo "</table>";
-?>  
-
-</body>
+                
+         ?>
+         </table>
+    </body>
 </html>
