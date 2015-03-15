270cff86f9c16d6a06d21b719ff722c49113e1fe
Henry0422 <ronglihan@Ronglis-MacBook-Pro.local>
2015-03-14 23:38:51 -0600

    Rewrite contact update test

--- update.php
+++ update.php
@@ -39,32 +39,35 @@
             }
 			
 			
-			
-			$sql = "Update AddressBook 
-				SET firstname = '".$_POST['fname']."',
-					lastname = '".$_POST['lname']."',
-					company = '".$_POST['company']."',
-					phone = '".$_POST['phone']."',
-					email = '".$_POST['email']."',
-					url = '".$_POST['url']."',
-					address = '".$_POST['address']."',";
-					
-			if(!empty($_POST["birthday"])){
-				$sql .= "birthday = '".$_POST['birthday']."',";
-			}
-					
-			$sql .=	"add_date = '".$_POST['add_date']."',
-						note ='".$_POST["note"]."'
-				WHERE id=".$_POST['contact_id'];
-				
-			if($conn->query($sql) == TRUE)
-				echo "<div class='alert alert-success' role='alert'><h1>Contact ".$_POST['fname']." ".$_POST['lname']." has been updated</h1></br>You are being redirected
+			try{
+                $sql = "Update AddressBook 
+                    SET firstname = '".$_POST['fname']."',
+                        lastname = '".$_POST['lname']."',
+                        company = '".$_POST['company']."',
+                        phone = '".$_POST['phone']."',
+                        email = '".$_POST['email']."',
+                        url = '".$_POST['url']."',
+                        address = '".$_POST['address']."',";
+
+                if(!empty($_POST["birthday"])){
+                    $sql .= "birthday = '".$_POST['birthday']."',";
+                }
+
+                $sql .=	"add_date = '".$_POST['add_date']."',
+                            note ='".$_POST["note"]."'
+                    WHERE id=".$_POST['contact_id'];
+                    $conn->query($sql);
+                
+                    echo "<div class='alert alert-success' role='alert'><h1>Contact ".$_POST['fname']." ".$_POST['lname']." has been updated</h1></br>You are being redirected
 						  <a href='home.php' class='alert-link'>Home.</a>
 						</div>";
-			else
-				echo "<div class='alert alert-danger' role='alert'>Error updating contact :".$e->getMessage()."
+            }
+
+            catch(PDOException $e){
+                echo "<div class='alert alert-danger' role='alert'>Error updating contact :".$e->getMessage()."
 						</div><a href='home.php' class='alert-link'>Go Home.</a>";
-				
+            }
+
 			header("Refresh: 5; url=home.php");
 		 ?> 
         
