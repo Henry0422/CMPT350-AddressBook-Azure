729d2599aa72104eecbc95d7681af866cca988fa
Henry0422 <ronglihan@uofs-10-227-161-152.usask.ca>
2015-03-17 19:35:33 -0600

    Add 403 for update.php

--- update.php
+++ update.php
@@ -38,35 +38,44 @@
                 die("Connection failed: ".print_r($e));
             }
 			
-			
-			try{
-                $sql = "Update AddressBook 
-                    SET firstname = '".$_POST['fname']."',
-                        lastname = '".$_POST['lname']."',
-                        company = '".$_POST['company']."',
-                        phone = '".$_POST['phone']."',
-                        email = '".$_POST['email']."',
-                        url = '".$_POST['url']."',
-                        address = '".$_POST['address']."',";
+			if($_POST['contact_id'] == ""){
+                echo "<div class='alert alert-danger' role='alert'>403 Forbidden<br>
+                    You are not able to access update.php directly</br>
+                    Automatically redirect to home page in 5 seconds</div>
+                    <a href='homePage.php' class='alert-link'>Go Home.</a>";
+            }
 
-                if(!empty($_POST["birthday"])){
-                    $sql .= "birthday = '".$_POST['birthday']."',";
+            else{
+                try{
+                    $sql = "Update AddressBook 
+                        SET firstname = '".$_POST['fname']."',
+                            lastname = '".$_POST['lname']."',
+                            company = '".$_POST['company']."',
+                            phone = '".$_POST['phone']."',
+                            email = '".$_POST['email']."',
+                            url = '".$_POST['url']."',
+                            address = '".$_POST['address']."',";
+
+                    if(!empty($_POST["birthday"])){
+                        $sql .= "birthday = '".$_POST['birthday']."',";
+                    }
+
+                    $sql .= "add_date = '".$_POST['add_date']."',
+                                note ='".$_POST["note"]."'
+                        WHERE id=".$_POST['contact_id'];
+                        $conn->query($sql);
+                    
+                        echo "<div class='alert alert-success' role='alert'><h1>Contact ".$_POST['fname']." ".$_POST['lname']." has been updated</h1></br>You are being redirected
+                              <a href='home.php' class='alert-link'>Home.</a>
+                            </div>";
                 }
 
-                $sql .=	"add_date = '".$_POST['add_date']."',
-                            note ='".$_POST["note"]."'
-                    WHERE id=".$_POST['contact_id'];
-                    $conn->query($sql);
-                
-                    echo "<div class='alert alert-success' role='alert'><h1>Contact ".$_POST['fname']." ".$_POST['lname']." has been updated</h1></br>You are being redirected
-						  <a href='home.php' class='alert-link'>Home.</a>
-						</div>";
+                catch(PDOException $e){
+                    echo "<div class='alert alert-danger' role='alert'>Error updating contact :".$e->getMessage()."
+                            </div><a href='home.php' class='alert-link'>Go Home.</a>";
+                }
             }
 
-            catch(PDOException $e){
-                echo "<div class='alert alert-danger' role='alert'>Error updating contact :".$e->getMessage()."
-						</div><a href='home.php' class='alert-link'>Go Home.</a>";
-            }
 
 			header("Refresh: 5; url=home.php");
 		 ?> 
